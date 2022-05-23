90d10ec0db3603e9f1c28b9a1363f38cc3e4ef72
Sumi <sumi.sastri@cancer.org.uk>
2022-05-23 16:15:52 +0100

    Set up Apollo client in root

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.css';
+import BookList from './components/BookList';
+
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +28,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>MERN Stack with GraphQL-Apollo Server</h1>
+      <BookList/>
     </div>
+    </ApolloProvider>
   );
 }
 
