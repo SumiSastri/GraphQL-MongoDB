065acbb23bb4fd51c25d9835f3eac3740e67abbf
Sumi <sumi.sastri@cancer.org.uk>
2022-05-30 21:23:53 +0100

    Remove comments from server-side schema

--- packages/server/schema/schema.js
+++ packages/server/schema/schema.js
@@ -2,10 +2,6 @@ const graphql = require('graphql');
 const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLInt, GraphQLID, GraphQLList, GraphQLNonNull } = graphql;
 
-// hardcoded data
-// const { Book, Author } = require("../mocks/index")
-
-// move data to db
 const Book = require ("../models/bookSchema")
 const Author = require ("../models/authorSchema")
 
@@ -14,7 +10,6 @@ const BookType = new GraphQLObjectType({
     name: 'Book',
     // void function to call the fields due to call-stack
     fields: () => ({
-        // id: { type: GraphQLString },
         id: { type: graphql.GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
@@ -23,11 +18,6 @@ const BookType = new GraphQLObjectType({
             type: AuthorType,
             resolve(parent, args) {
                 return Author.findById(parent.authorId);
-                // FROM HARD-CODED DATA ARRAY
-                //  console.log(parent)
-                // return _.find(authors, { id: parent.authorId });
-                // with ES6
-                // return authors.find(author => author.id === parent.authorId)
             }
         }
     })
@@ -38,23 +28,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: graphql.GraphQLID },
         name: { type: GraphQLString },
-        century: { type: GraphQLInt },
-        // Link one author to a book
-        // books: {
-        //     type: BookType
-        // }
+        century: { type: GraphQLInt },    
         // Link author to a list of books with a constructor
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
                 // mongoose method
                 return Book.find({ authorId: parent.id });
-                      // FROM HARD-CODED DATA ARRAY
-                // console.log(parent)
-                // return _.filter(books, {authorId: parent.id})
-                // with ES6
-                // return books.filter(book => book.authorId === parent.id)
-
             }
         }
     })
@@ -71,12 +51,6 @@ const RootQuery = new GraphQLObjectType({
             // get request server-side data - source agnostic
             resolve(parent, args) {
                 return Book.findById(args.id);
-                      // FROM HARD-CODED DATA ARRAY
-                // console.log(parent)
-                // return _.find(books, { id: args.id });
-                // with ES6
-                // return books.find(book => book.id === args.id)
-
             }
         },
         // author object
@@ -85,11 +59,6 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 return Author.findById(args.id);
-                      // FROM HARD-CODED DATA ARRAY
-                // console.log(parent)
-                // with ES6
-                // return authors.find(author => author.id === args.id)
-
             }
         },
         // books and authors as lists
@@ -112,7 +81,8 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 });
-      // FROM & TO DB 
+
+// FROM & TO DB 
 // mutations are the equivalent of the CRUD actions - add, update, create, delete
 // findByIdAndUpdate() findByIdAndRemove() - mongoose methods
 const Mutation = new GraphQLObjectType({
@@ -122,9 +92,6 @@ const Mutation = new GraphQLObjectType({
             type: AuthorType,
             // ensure the types are typed correctly 
             args: {
-                  // VALIDATION: NonNull makes the key-value pair required
-                // name: { type: GraphQLString },
-                // century: { type: GraphQLInt },
                 name: { type: new GraphQLNonNull(GraphQLString) },
                 century: { type: new GraphQLNonNull(GraphQLInt) },
             },
@@ -135,9 +102,6 @@ const Mutation = new GraphQLObjectType({
                     name: args.name,
                     century: args.century
                 });
-                // mongoose save method - saves data to db
-                // author.save()
-                // if you do not return it saves it on db but does not return it to fe
                 return author.save()
             }
         },
@@ -145,9 +109,6 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                // name: { type: GraphQLString },
-                // genre: { type: GraphQLString },
-                // authorId: { type: GraphQLID }
                 name: { type: new GraphQLNonNull(GraphQLString) },
                 genre: { type: new GraphQLNonNull(GraphQLString) },
                 authorId: { type: new GraphQLNonNull(GraphQLID) }
@@ -164,7 +125,6 @@ const Mutation = new GraphQLObjectType({
     }
 });
 
-// queries and mutations exported for reuse (Commonjs)
 module.exports = new GraphQLSchema({
     query: RootQuery,
     mutation: Mutation
